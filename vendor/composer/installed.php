5bfa962d7d0dbdc18d6d522d930d21791698f1b2
zhyar99 <zhyar.9991@gmail.com>
2022-01-26 10:13:53 +0300

    changes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '83afd7bdaa000849c57d2fc8d697e803e5182114',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '83afd7bdaa000849c57d2fc8d697e803e5182114',
+    ),
+    'heroku/heroku-buildpack-php' => 
+    array (
+      'pretty_version' => 'v205',
+      'version' => '205.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8937f4b1ab1697b12c06369ed3b0eada2525ee94',
+    ),
+  ),
+);
