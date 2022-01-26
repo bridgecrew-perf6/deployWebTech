61cff9cf903eeb00998b5f4b2bea2af2c88156bf
zhyar99 <zhyar.9991@gmail.com>
2022-01-26 10:43:48 +0300

    asd

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,28 +6,19 @@
     'aliases' => 
     array (
     ),
-    'reference' => '83afd7bdaa000849c57d2fc8d697e803e5182114',
-    'name' => '__root__',
+    'reference' => '7d91844b149b31c10614c9d50d5d4fae1aa4b73d',
+    'name' => 'deploy-web-tech/test',
   ),
   'versions' => 
   array (
-    '__root__' => 
+    'deploy-web-tech/test' => 
     array (
       'pretty_version' => 'dev-main',
       'version' => 'dev-main',
       'aliases' => 
       array (
       ),
-      'reference' => '83afd7bdaa000849c57d2fc8d697e803e5182114',
-    ),
-    'heroku/heroku-buildpack-php' => 
-    array (
-      'pretty_version' => 'v205',
-      'version' => '205.0.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '8937f4b1ab1697b12c06369ed3b0eada2525ee94',
+      'reference' => '7d91844b149b31c10614c9d50d5d4fae1aa4b73d',
     ),
   ),
 );
